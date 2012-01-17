a5ab8e87bbac6f7c1eff019f5ae60d135aa75caf
Timothy Lebo <timrdf@gmail.com>
2012-01-17 11:25:54 -0500

    stub configure brick ui

---
+++ ui/configure-epoch/index.php
@@ -0,0 +1,20 @@
+<html> 
+    <head>                                                                  
+    <script type="text/javascript" src="jquery-1.7.1.js"></script>          
+    <script type="text/javascript">                                         
+
+$(document).ready(function() {
+
+   $("a").click(function() {
+      alert("Hello world!");
+   });
+
+});
+
+    </script>                                                               
+    </head>                                                              
+    <body>                                                               
+         <a href="">Link</a>
+    </body>                                                              
+</html>
+
