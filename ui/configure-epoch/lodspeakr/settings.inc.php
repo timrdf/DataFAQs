a6331d435bbc4dff843218c8c0b001c8bf2fe486
Timothy Lebo <timrdf@gmail.com>
2012-01-17 19:01:50 -0500

    stub

---
+++ ui/configure-epoch/lodspeakr/settings.inc.php
@@ -0,0 +1,19 @@
+<?
+
+$conf['endpoint']['local'] = 'http://sparql.tw.rpi.edu:3030/datafaqs/query';
+$conf['home'] = '/Applications/XAMPP/xamppfiles/htdocs/hello/lodspeakr/';
+$conf['basedir'] = 'http://localhost/hello/';
+$conf['debug'] = false;
+$conf['mirror_external_uris'] = true;
+
+/*ATTENTION: By default this application is available to
+ * be exported and copied (its configuration)
+ * by others. If you do not want that, 
+ * turn the next option as false
+ */ 
+$conf['export'] = true;
+
+#If you want to add/overrid a namespace, add it here
+$conf['ns']['local']   = 'http://sparql.tw.rpi.edu/datafaqs/';
+$conf['ns']['base']   = 'http://localhost/hello/';
+?>
