4caa8cce77be5998ebe7d3257f5cca4cc6890446
Timothy Lebo <timrdf@gmail.com>
2012-01-21 13:17:33 -0500

    situating paths

--- bin/df-epoch.sh
+++ bin/df-epoch.sh
@@ -15,7 +15,10 @@ DATAFAQS_HOME=${DATAFAQS_HOME:?"not set; see https://github.com/timrdf/DataFAQs/
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 DATAFAQS_BASE_URI=${DATAFAQS_BASE_URI:?"not set; see https://github.com/timrdf/DataFAQs/wiki/DATAFAQS-envrionment-variables"}
 
+echo $PATH
 export PATH=$PATH`$DATAFAQS_HOME/bin/df-situate-paths.sh`
+echo
+echo $PATH
 
 metadata_name=${DATAFAQS_PUBLISH_METADATA_GRAPH_NAME:-'http://www.w3.org/ns/sparql-service-description#NamedGraph'}
 
