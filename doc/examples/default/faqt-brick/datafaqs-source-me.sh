579e294edb5160b719876618a481964bb6e82633
Timothy Lebo <timrdf@gmail.com>
2012-02-09 21:23:34 -0500

    added virtuoso vars

--- doc/examples/default/faqt-brick/datafaqs-source-me.sh
+++ doc/examples/default/faqt-brick/datafaqs-source-me.sh
@@ -2,18 +2,30 @@ export DATAFAQS_HOME="/opt/DataFAQs"
 export PATH=$PATH`$DATAFAQS_HOME/bin/df-situate-paths.sh`
 
 # Project settings
-export DATAFAQS_BASE_URI="http://aquarius.tw.rpi.edu/projects/datafaqs"
 export DATAFAQS_LOG_DIR="`pwd`/log"
-export DATAFAQS_PUBLISH_TDB="true"
-export DATAFAQS_PUBLISH_TDB_DIR="`pwd`/tdb"
+export DATAFAQS_BASE_URI=""
 export DATAFAQS_PUBLISH_THROUGHOUT_EPOCH="true"
 export DATAFAQS_PUBLISH_METADATA_GRAPH_NAME="http://www.w3.org/ns/sparql-service-description#NamedGraph"
+
+# If using TDB:
+export DATAFAQS_PUBLISH_TDB="true"
+export DATAFAQS_PUBLISH_TDB_DIR="`pwd`/tdb"
+export TDBROOT="/opt/tdb/TDB-0.8.10"
+if [ ! `which tdbloader` ]; then
+   export PATH=$PATH":$TDBROOT/bin"
+fi
+
+# If using Virtuoso:
+export DATAFAQS_PUBLISH_VIRTUOSO='true'
+export CSV2RDF4LOD_CONVERT_DATA_ROOT="`pwd`"
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO_HOME='/opt/virtuoso'
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO_ISQL_PATH='' # defaults to guess
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO_PORT=1111
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO_USERNAME='dba'
+export CSV2RDF4LOD_PUBLISH_VIRTUOSO_PASSWORD='your-virtuoso-password'
 
 # Software dependencies: 
 export CSV2RDF4LOD_HOME="/opt/csv2rdf4lod-automation"
 export PATH=$PATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-paths.sh`
 
-export TDBROOT="/opt/tdb/TDB-0.8.10"
-if [ ! `which tdbloader` ]; then
-   export PATH=$PATH":$TDBROOT/bin"
-fi
+df-vars.sh
