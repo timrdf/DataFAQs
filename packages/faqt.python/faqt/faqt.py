5e3aae9c283e72e843fc3b14e37ee1887290174f
Timothy Lebo <timrdf@gmail.com>
2012-05-19 17:01:46 -0400

    faqt.python

--- packages/faqt.python/faqt/faqt.py
+++ packages/faqt.python/faqt/faqt.py
@@ -51,7 +51,7 @@ class Service(sadi.Service):
 
       self.servicePath    = servicePath # No ending slash
 
-      self.uuid           = uuid.UUID()
+      self.uuid           = uuid.uuid1()
       self.startedLifeAt  = datetime.datetime.utcnow()
 
       self.baseURI        = os.environ['DATAFAQS_BASE_URI']                  if 'DATAFAQS_BASE_URI'                  in os.environ \
