18fbdcb49ccc7f621a508e78a60098cdfee70ac0
Timothy Lebo <timrdf@gmail.com>
2012-05-19 15:04:28 -0400

    faqt.python

--- packages/faqt.python/faqt/faqt.py
+++ packages/faqt.python/faqt/faqt.py
@@ -66,7 +66,7 @@ class Service(sadi.Service):
       Page        = desc.session.get_class(ns.FOAF['Page'])
 
       desc.dcterms_subject.append(Agent(''))
-      desc.rdfs_comment.append(self.servicePath)
+      desc.datafaqs_comment.append(self.servicePath)
 
       agent = Agent('')
       #agent.prov_generatedAtTime.append(self.startedLifeAt);
