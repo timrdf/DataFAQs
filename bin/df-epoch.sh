1ff108d46eeba868197be0e72d3762ab7367ccb6
Timothy Lebo <timrdf@gmail.com>
2012-05-22 10:32:47 -0400

    epoch

--- bin/df-epoch.sh
+++ bin/df-epoch.sh
@@ -500,13 +500,13 @@ if [ "$epoch_existed" != "true" ]; then
                source get-$file.sh
                triples=`void-triples.sh $file`
                mime=`guess-syntax.sh --inspect "$file" mime`
-               head -1 $file | awk -v indent="$indent" -v triples=$triples -v mime=$mime '{print indent"     "$0" ("triples" triples in "mime")"}'
+               head -1 $file | awk -v indent="$indent" -v triples=$triples -v mime=$mime '{print indent"     "$0" ("triples" "mime" triples"}'
                extension=`guess-syntax.sh --inspect "$file" extension`
                blah=`$CSV2RDF4LOD_HOME/bin/util/rename-by-syntax.sh $file`                                                    # part-{1,2,3,...}.{ttl,rdf,nt}
                if [ $triples -gt 0 ]; then
                   rapper -q -g -o turtle $file.$extension                                                                    >> post.ttl
                fi
-               indent="    "
+               indent="     "
                let 's=s+1'
             done
 
