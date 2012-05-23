ee6e448fcc328e89cfd533bf227c56c5144f9e28
Timothy Lebo <timrdf@gmail.com>
2012-05-23 09:39:25 -0400

    references and augmenters

--- bin/df-epoch.sh
+++ bin/df-epoch.sh
@@ -493,11 +493,11 @@ if [ "$epoch_existed" != "true" ]; then
          pushd __PIVOT_dataset/$datasetDir &> /dev/null
 
             # Set up (and submit) requests for references (from the referencers).
-            r=0 "referencer"
+            r=0 # "referencer"
             for referencer in `cat $epochDir/referencers.csv`; do 
                let 'r=r+1'
                echo "curl -s -H 'Content-Type: text/turtle' -d @dataset.ttl $referencer > references-$r"   > get-references-$r.sh
-               source get-references-$r.sh
+                                                                                                      source get-references-$r.sh
                file=`$CSV2RDF4LOD_HOME/bin/util/rename-by-syntax.sh --verbose references-$r`
                if [ `void-triples.sh $file` -gt 0 ]; then
                   rapper -q -g -o ntriples $file                                                          >> references.nt
