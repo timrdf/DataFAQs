2be73d891b08dfe9d36cac605026415fafa04233
Timothy Lebo <timrdf@gmail.com>
2012-12-22 05:32:34 -0500

    handling XML results in addition to JSON

--- bin/df-named-graphs.py
+++ bin/df-named-graphs.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+#
+# <> prov:specializationOf <https://github.com/timrdf/DataFAQs/blob/master/bin/df-named-graphs.py> .
 
 import sys
 from rdflib import *
@@ -12,21 +14,31 @@ rdflib.plugin.register('sparql', rdflib.query.Processor,
 rdflib.plugin.register('sparql', rdflib.query.Result,
                        'rdfextras.sparql.query', 'SPARQLQueryResult')
 
+import xml.dom
+
 ns.register(prov='http://www.w3.org/ns/prov#')
 ns.register(dcat='http://www.w3.org/ns/dcat#')
 ns.register(void='http://rdfs.org/ns/void#')
 ns.register(datafaqs='http://purl.org/twc/vocab/datafaqs#')
 
 if len(sys.argv) < 2:
-   print "usage: df-named-graphs.py endpoint"
-   sys.exit
+   print "usage: df-named-graphs.py <endpoint>"
+   sys.exit()
 
 endpoint = sys.argv[1]
 
 store = Store( reader = "sparql_protocol",
                writer = "sparql_protocol",
                endpoint = endpoint)
-results = store.execute_sparql('select distinct ?g where { graph ?g {[] a ?type}} order by ?g')
+#results = store.execute_sparql('select distinct ?g where { graph ?g {[] a ?type}} order by ?g')
+results = store.execute_sparql('select distinct ?g where { graph ?g {[] a ?type}}')
 
-for result in results['results']['bindings']:
-   print result['g']['value']
+if isinstance(results,xml.dom.minidom.Document):
+   for result in results.getElementsByTagName('result'):
+      for binding in result.getElementsByTagName('binding'):
+         for value in result.getElementsByTagName('uri'):
+            print value.firstChild.data
+else:
+   # If JSON results:
+   for result in results['results']['bindings']:
+      print result['g']['uri']
