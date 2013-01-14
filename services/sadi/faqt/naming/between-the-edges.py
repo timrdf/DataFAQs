1868c9ae6d995921965543c607fe38e4ec26ed61
Timothy Lebo <timrdf@gmail.com>
2013-01-14 08:41:48 -0500

    stab at BTE.py

--- services/sadi/faqt/naming/between-the-edges.py
+++ services/sadi/faqt/naming/between-the-edges.py
@@ -1,4 +1,4 @@
-#3> <> prov:specializationOf <#TEMPLATE/path/to/public/source-code.py>;
+#3> <> prov:specializationOf <https://github.com/timrdf/DataFAQs/blob/master/services/sadi/faqt/naming/between-the-edges.py>;
 #3>    rdfs:seeAlso <https://github.com/timrdf/DataFAQs/wiki/FAqT-Service> .
 
 import faqt
@@ -30,6 +30,7 @@ ns.register(dcat='http://www.w3.org/ns/dcat#')
 ns.register(sd='http://www.w3.org/ns/sparql-service-description#')
 ns.register(conversion='http://purl.org/twc/vocab/conversion/')
 ns.register(datafaqs='http://purl.org/twc/vocab/datafaqs#')
+ns.register(bte='http://purl.org/twc/vocab/between-the-edges/')
 
 # The Service itself
 class BetweenTheEdges(faqt.Service):
@@ -61,25 +62,72 @@ class BetweenTheEdges(faqt.Service):
       return result
 
    def getInputClass(self):
-      return ns.DCAT['Dataset']
+      return ns.RDFS['Resource']
 
    def getOutputClass(self):
-      return ns.DATAFAQS['EvaluatedDataset']
+      return ns.BTE['RDFNode']
 
+   @staticmethod
+   def length(input,output):
+      output.bte_length = len(input.subject)
+      
+   @staticmethod
+   def scheme(url6,output):
+      if len(url6.scheme):
+         output.bte_scheme = url6.scheme
+      
+   @staticmethod
+   def netloc(url6,output):
+      if len(url6.netloc):
+         output.bte_netloc = url6.netloc
+      
+   @staticmethod
+   def path(url6,output):
+      if len(url6.path):
+         output.bte_path = url6.path
+      
+   @staticmethod
+   def fragment(url6,output):
+      if len(url6.fragment):
+         output.bte_fragment = url6.fragment
+      
    def process(self, input, output):
 
       print 'processing ' + input.subject
 
+      length = BetweenTheEdges.length(input,output)
+
+      #
+      # Using urlparse
+      # e.g. ParseResult(scheme='http', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html', params='', query='', fragment='')
+      #
+      url6 = urlparse(str(input.subject))
+      scheme   = BetweenTheEdges.scheme(url6,output)
+      netloc   = BetweenTheEdges.netloc(url6,output)
+      path     = BetweenTheEdges.path(url6,output)
+      fragment = BetweenTheEdges.fragment(url6,output)
+
+      # <http://dailymed.nlm.nih.gov/dailymed/help.cfm#webservices> 
+      #    a bte:RDFNode;
+      #    bte:scheme "http";
+      #    bte:netloc      "dailymed.nlm.nih.gov";
+      #    bte:path                             "/dailymed/help.cfm";
+      #    bte:fragment                                            "webservices";
+      #    bte:length   57;
+
+      #if scheme in ['http']:
+      #   BetweenTheEdges.walkPath(path,outout)
+
       ####
       # Query a SPARQL endpoint
-      store = Store(reader = 'sparql_protocol', endpoint = 'http://dbpedia.org/sparql')
-      session = Session(store)
-      session.enable_logging = False
-      result = session.default_store.execute_sparql('select distinct ?type where {[] a ?type} limit 2')
-      if result:
-         for binding in result['results']['bindings']:
-            type  = binding['type']['value']
-            print type
+      #store = Store(reader = 'sparql_protocol', endpoint = 'http://dbpedia.org/sparql')
+      #session = Session(store)
+      #session.enable_logging = False
+      #result = session.default_store.execute_sparql('select distinct ?type where {[] a ?type} limit 2')
+      #if result:
+      #   for binding in result['results']['bindings']:
+      #      type  = binding['type']['value']
+      #      print type
       ####
 
       # Query the RDF graph POSTed: input.session.default_store.execute
@@ -91,12 +139,7 @@ class BetweenTheEdges(faqt.Service):
       # Create a calss in the output graph:
       # Document = output.session.get_class(ns.FOAF['Document'])
 
-      if True:
-         output.rdf_type.append(ns.DATAFAQS['Unsatisfactory'])
- 
-      if ns.DATAFAQS['Unsatisfactory'] not in output.rdf_type:
-         output.rdf_type.append(ns.DATAFAQS['Satisfactory'])
-
+      output.rdf_type.append(ns.BTE['RDFNode'])
       output.save()
 
 # Used when Twistd invokes this service b/c it is sitting in a deployed directory.
