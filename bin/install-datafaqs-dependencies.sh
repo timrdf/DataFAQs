f048f36e5f9537f43ec4986609bccfa6a073364a
Timothy Lebo <timrdf@gmail.com>
2013-02-10 09:55:42 -0500

    reconscile with prizms install

--- bin/install-datafaqs-dependencies.sh
+++ bin/install-datafaqs-dependencies.sh
@@ -1,37 +1,107 @@
 #!/bin/bash
-# bin/install-datafaqs-dependencies.sh
 #
-# https://github.com/timrdf/DataFAQs/blob/master/bin/install-datafaqs-dependencies.sh
+#3> <> prov:specializationOf <https://github.com/timrdf/DataFAQs/blob/master/bin/install-datafaqs-dependencies.sh>
+
+if [[ "$1" == "--help" ]]; then
+   echo
+   echo "usage: `basename $0` [-n] [--avoid-sudo]"
+   echo
+   echo "  Install the third-party utilities that DataFAQs uses."
+   #echo "  Will install everything relative to the path:"
+   #echo "     $base"
+   echo "  See https://github.com/timrdf/DataFAQs/wiki/Installing-DataFAQs"
+   echo
+   echo "   -n          | Perform only a dry run. This can be used to get a sense of what will be done before we actually do it."
+   echo
+   echo "  --avoid-sudo : Avoid using sudo if at all possible. It's best to avoid root."
+   echo
+   exit
+fi
+
+dryrun="false"
+TODO=''
+if [ "$1" == "-n" ]; then
+   dryrun="true"
+   #$sibling/dryrun.sh $dryrun beginning
+   TODO="[TODO]"
+   shift
+fi
 
 function offer_install_with_apt {
+   # See also https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/install-csv2rdf4lod-dependencies.sh
+   # See also https://github.com/timrdf/DataFAQs/blob/master/bin/install-datafaqs-dependencies.sh
+
    command="$1"
    package="$2"
    if [ `which apt-get` ]; then
-      if [[ ${#command} -gt 0 && ${#package} -gt 0 ]]; then
+      if [[ -n "$command" && -n "$package" ]]; then
          if [ ! `which $command` ]; then
-            echo
-            echo sudo apt-get install $package
-            echo -n "Could not find $command on path. Try to install with command shown above? (y/n): "
-            read -u 1 install_it
-            if [ "$install_it" == "y" ]; then
-               echo sudo apt-get install $package
-               sudo apt-get install $package
-            fi 
+            if [ "$dryrun" != "true" ]; then
+               echo
+            fi
+            echo $TODO $sudo apt-get install $package
+            if [ "$dryrun" != "true" ]; then
+               read -p "Could not find $command on path. Try to install with command shown above? (y/n): " -u 1 install_it
+               if [[ "$install_it" == [yY] ]]; then
+                  echo $sudo apt-get install $package
+                       $sudo apt-get install $package
+               fi
+            fi
          else
-            echo "$command available at `which $command`"
+            echo "[okay] $command already available at `which $command`"
          fi
       fi
    else
-      echo "Sorry, we need apt-get to install $command / $package for you."
+      echo "[WARNING] Sorry, we need apt-get to install $command / $package for you."
    fi
+   which $command >& /dev/null
+   return $?
 }
 
-offer_install_with_apt 'git'          'git-core'
+if [ "$dryrun" != "true" ]; then
+   $sudo apt-get update &> /dev/null
+fi
+
+offer_install_with_apt 'git'          'git-core' # These are dryrun safe.
 offer_install_with_apt 'curl'         'curl'
 offer_install_with_apt 'rapper'       'raptor-utils'
 offer_install_with_apt 'unzip'        'unzip'
 offer_install_with_apt 'easy_install' 'python-setuptools'
 offer_install_with_apt 'sqlite3'      'sqlite3 libsqlite3-dev'
+
+offer_install_with_apt 'easy_install' 'python-setuptools' # dryrun aware
+V=`python --version 2>&1 | sed 's/Python \(.\..\).*$/\1/'`
+eggs="pyparsing surf.sparql_protocol ckanclient BeautifulSoup"
+for egg in $eggs; do
+   # See also https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/install-csv2rdf4lod-dependencies.sh
+   # See also https://github.com/timrdf/DataFAQs/blob/master/bin/install-datafaqs-dependencies.sh
+   eggReg=`echo $egg | sed 's/-/./g;s/_/./g'`
+   find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 | grep -i $eggReg &> /dev/null
+   status=$?
+   there=`find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 -type d | grep -i $eggReg`
+   if [[ "$there" =~ /usr/*.egg ]]; then # TODO: this path is $base/python/lib/site-packages if -z $sudo
+      # TODO: not recognizing that 'ckanclient' is missing.
+      echo $pdiv
+      echo $TODO $sudo easy_install -U $egg
+      if [ "$dryrun" != "true" ]; then
+         read -p "Try to install python module $egg using the command above? (y/n) " -u 1 install_it
+         if [[ "$install_it" == [yY] ]]; then
+                 $sudo easy_install -U $egg
+                # SUDO IS NOT REQUIRED HERE.
+            # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation---complete
+            pdiv=""
+         fi
+      fi
+   else
+      echo "[okay] python egg \"$egg\" is already available at $there ($eggReg $status)"
+   fi
+done
+
+
+
+exit
+
+# This stuff doesn't check to see if it's already there, so was replaced by the loop above (which is shared with the csv2rdf4lod installer)
 
 echo
 echo -n "Try to install python libraries? (y/N) "
