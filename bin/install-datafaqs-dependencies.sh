c2b1883186bf9b2616adff62ae729ab302e9d447
Tim L <timrdf@gmail.com>
2013-04-11 18:42:01 -0400

    java sadi tomcat

--- bin/install-datafaqs-dependencies.sh
+++ bin/install-datafaqs-dependencies.sh
@@ -131,7 +131,7 @@ if [ "$dryrun" == "true" ]; then
    echo $HOME >&2
    echo $me >&2
    ls /var/lib/tomcat6/webapps/ >&2
-   du -sh $HOME/services/sadi/sadi-services.war
+   du -sh $HOME/services/sadi/sadi-services.war >&2
 fi
 
 
