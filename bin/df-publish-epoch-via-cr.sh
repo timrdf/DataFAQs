96f091e76075c7f4568ce5b63815e6a2a111809d
Tim L <timrdf@gmail.com>
2013-06-22 15:12:35 -0400

    publish faqt brick epoch as cr versioned dataset

--- bin/df-publish-epoch-via-cr.sh
+++ bin/df-publish-epoch-via-cr.sh
@@ -29,7 +29,8 @@ fi
 
 for posted in `find __PIVOT_epoch/$epochID/__PIVOT_dataset -name post.nt.rdf`; do
    md5=`md5.sh -qs "$posted"`
-   ln -s $posted ../$epochID/source/posted-$md5.rdf
+   echo ln -s $posted ../$epochID/source/posted-$md5.rdf
+        ln -s $posted ../$epochID/source/posted-$md5.rdf
 done
 
 find __PIVOT_faqt -name evaluation.rdf | grep __PIVOT_epoch/$epochID
