ac2a416856dd17215c1764aa856aceb785150f09
Tim L <timrdf@gmail.com>
2014-07-07 14:41:50 -0400

    handling naming for datasets, groups, tags

--- src/java/sadi-services/src/main/java/edu/rpi/tw/data/ckan/CKANReader.java
+++ src/java/sadi-services/src/main/java/edu/rpi/tw/data/ckan/CKANReader.java
@@ -1,5 +1,7 @@
 package edu.rpi.tw.data.ckan;
 
+import org.apache.log4j.Logger;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -13,6 +15,8 @@ import edu.rpi.tw.data.rdf.jena.vocabulary.PROV;
  */
 public class CKANReader {
 	
+   private static final Logger log = Logger.getLogger(CKANReader.class);
+   
 	/**
 	 * e.g. http://datahub.io/dataset/farmers-markets-geographic-data-united-states -> http://datahub.io
 	 * 
@@ -20,7 +24,9 @@ public class CKANReader {
 	 * @return
 	 */
 	public static String getBaseURI(Resource dataset) {
-		return dataset.asResource().getURI().toString().replaceFirst("\\/dataset\\/.*$", "");
+		return dataset.asResource().getURI().toString().replaceFirst("\\/dataset\\/.*$", "")
+		      .replaceFirst("\\/group\\/.*$", "")
+		      .replaceFirst("\\/tag\\/.*$", "");
 	}
 	
 	/** 
@@ -42,14 +48,22 @@ public class CKANReader {
 		if( base == null && datasetS.matches("^.*/dataset/.*") ) {
 
 			base = datasetS.replaceFirst("/dataset/.*$", "");
-		}
+		}else if( base == null && datasetS.matches("^.*/group/.*") ) {
+
+         base = datasetS.replaceFirst("/group/.*$", "");
+      }
+		else if( base == null && datasetS.matches("^.*/tag/.*") ) {
+
+         base = datasetS.replaceFirst("/tag/.*$", "");
+      }
+		log.warn("ckan API of "+dataset.toString()+" : "+base);
 		return base != null ? base : null;
 	}
 	
 	/**
 	 * 'http://datahub.io/dataset/farmers-markets-geographic-data-united-states' -> 'farmers-markets-geographic-data-united-states'
 	 */
-	public static String getCKANIdentiifer(Resource dataset) {
+	public static String getCKANIdentifier(Resource dataset) {
 		
 		if( dataset.hasProperty(DataFAQs.ckan_identifier) ) { 
 			return dataset.getProperty(DataFAQs.ckan_identifier).getObject().asLiteral().getString();
@@ -60,7 +74,13 @@ public class CKANReader {
 		}else if( dataset.asResource().getURI().matches("^.*/dataset/.*") ) {
 			return dataset.asResource().getURI().replaceAll("^.*/dataset/","");
 			
-		}
+		}else if( dataset.asResource().getURI().matches("^.*/group/.*") ) {
+         return dataset.asResource().getURI().replaceAll("^.*/group/","");
+         
+      }else if( dataset.asResource().getURI().matches("^.*/tag/.*") ) {
+         return dataset.asResource().getURI().replaceAll("^.*/tag/","");
+         
+      }
 		return dataset.asResource().getURI().replaceAll("^.*/dataset/", "");
 	}
 	
@@ -73,7 +93,7 @@ public class CKANReader {
 		System.out.println(CKANReader.getCKANAPI(m.createResource("http://healthdata.tw.rpi.edu/hub/dataset/2010-basic-stand-alone-home")));
 		System.out.println(CKANReader.getCKANAPI(m.createResource("http://datahub.io/dataset/farmers-markets-geographic-data-united-states")));
 		
-		System.out.println(CKANReader.getCKANIdentiifer(m.createResource("http://thedatahub.org/dataset/uk-postcodes")));
-		System.out.println(CKANReader.getCKANIdentiifer(m.createResource("http://datahub.io/dataset/klappstuhlclub")));
+		System.out.println(CKANReader.getCKANIdentifier(m.createResource("http://thedatahub.org/dataset/uk-postcodes")));
+		System.out.println(CKANReader.getCKANIdentifier(m.createResource("http://datahub.io/dataset/klappstuhlclub")));
 	}
 }
