18632d86d2b8d785a5c9f42d3176f707fb8c14df
Tim L <timrdf@gmail.com>
2014-07-07 12:26:04 -0400

    stub implementation for by ckan tag (not verified as working)

---
+++ src/java/sadi-services/src/main/java/edu/rpi/tw/data/quality/sadi/faqt/core/select/datasets/ByCKANTag.java
@@ -0,0 +1,63 @@
+package edu.rpi.tw.data.quality.sadi.faqt.core.select.datasets;
+
+import org.apache.log4j.Logger;
+import org.ckan.CKANException;
+import org.ckan.Client;
+import org.ckan.Connection;
+import org.ckan.Dataset;
+
+import ca.wilkinsonlab.sadi.service.annotations.ContactEmail;
+import ca.wilkinsonlab.sadi.service.annotations.Description;
+import ca.wilkinsonlab.sadi.service.annotations.InputClass;
+import ca.wilkinsonlab.sadi.service.annotations.Name;
+import ca.wilkinsonlab.sadi.service.annotations.OutputClass;
+import ca.wilkinsonlab.sadi.service.simple.SimpleSynchronousServiceServlet;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DCTerms;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+import edu.rpi.tw.data.ckan.CKANReader;
+import edu.rpi.tw.data.rdf.jena.vocabulary.DCAT;
+import edu.rpi.tw.data.rdf.jena.vocabulary.DataFAQs;
+import edu.rpi.tw.data.rdf.jena.vocabulary.Prefixes;
+
+@Name("select-datasets-by-ckan-tag")
+@Description("Links a CKAN tag to the datasets that are in the tag.")
+@ContactEmail("lebot@rpi.edu")
+@InputClass("http://www.w3.org/ns/prov#Entity")
+@OutputClass("http://purl.org/twc/vocab/datafaqs#DatasetCollection")
+public class ByCKANTag extends SimpleSynchronousServiceServlet {
+   
+	private static final Logger log = Logger.getLogger(ByCKANTag.class);
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void processInput(Resource input, Resource output) {
+	   
+      String inputS = input.asResource().getURI().toString();
+      log.warn("processing "+inputS);
+      String tagID = CKANReader.getCKANIdentifier(input);
+      log.warn("processing id "+tagID);
+      Model m = output.getModel();
+      Prefixes.setNsPrefixes(m);
+      
+      String base = CKANReader.getBaseURI(input);
+      // (Taken from LiftCKAN)
+      Client client = new Client( new Connection( CKANReader.getCKANAPI(input) ), "");
+      //                                          ^ e.g. "http://datahub.io"     <key>
+      
+      try {
+         output.addProperty(RDF.type, DataFAQs.DatasetCollection);
+         for( Dataset dataset : client.findDatasetsByTag(tagID).results ) {
+            //log.warn(groupID + " has dataset "+dataset.getName());
+            Resource datasetR = m.createResource(base+"/dataset/"+dataset.getName(), DataFAQs.CKANDataset);
+            datasetR.addProperty(RDF.type, DCAT.Dataset);
+            output.addProperty(DCTerms.hasPart, datasetR);
+         }
+      }catch (CKANException e) {
+         e.printStackTrace();
+      }
+	}
+}
